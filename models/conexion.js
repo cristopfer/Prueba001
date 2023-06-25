c20f27864ed3ebabd25a59029ce9468773298989
cristopfer <eysen.perez1@unmsm.edu.pe>
2023-06-24 20:39:59 -0700

    y una vez commit

--- models/conexion.js
+++ models/conexion.js
@@ -12,7 +12,8 @@ var config = {
     database: 'pgscse',
     password: 'vkP0DchOI6a50Se6UrWd67jf7h9MggjP',
     host: 'dpg-cibnjjh5rnuk9q9sfqdg-a.oregon-postgres.render.com',
-    port: 5432
+    port: 5432,
+    ssl: true
 };
 var pool = new Pool(config);
 //const pool = await poolPromise;
