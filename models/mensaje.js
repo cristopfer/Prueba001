7acaa3e883ebfbe0cfebf000c2f00d983c87ad95
cristopfer <eysen.perez1@unmsm.edu.pe>
2023-06-29 02:31:36 -0700

    subiendo3

--- models/mensaje.js
+++ models/mensaje.js
@@ -30,8 +30,6 @@ function ConsultarMensajeProfesor(idUsuario,idProfesor,respuesta){
 
 function ConsultarMensajePrivado(idProfesor,respuesta){ 
     pool.connect(function(err, client, done) { 
-        console.log("aqui");
-        console.log(idProfesor);
         client.query("SELECT * FROM consultar_mensaje_privado_profesor($1)", [idProfesor], function(err,data) {
             done(); 
             if(err){
